042f76ddcba79aa7c8e09391513603e2f50a9021
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2017-11-01 15:27:11 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+```javascript
+setTransaction(formData: any){
+
+    console.log(formData);
+
+    if (!localStorage.getItem('transaction')) {
+
+        return localStorage.setItem('transaction', JSON.stringify(formData));
+
+    }else{
+
+
+        var trn = this.getTransactions(); // Get the object from the local storage.
+
+        if(formData.company && formData.address){
+          return localStorage.setItem('transaction', JSON.stringify(formData)); // Set the form data into the local storage.          
+        }else{
+
+          var mrgObj = Object.assign(formData, trn); // Merge the two objects (the form data and the object from the local storage)
+          
+          return localStorage.setItem('transaction', JSON.stringify(mrgObj)); // Set the form data into the local storage.
+          
+
+        }
+        
+    }
+    ```
